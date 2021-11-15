bf852ecd098baff8835ac36e1bee7efe7f9376de
pkryazhev <pkryazhev@ozon.ru>
2021-11-15 22:26:51 +0300

    step 1. Add prettier

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,8 +3,10 @@ import { Command } from 'commander/esm.mjs';
 const run = () => {
   const program = new Command();
   program
-      .description('Compares two configuration files and shows a difference.')
-      .option('-f, --format [type]', 'output format')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .version('0.0.1');
+  program.parse(process.argv);
 };
 
 export default run;
