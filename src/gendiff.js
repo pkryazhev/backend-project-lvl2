ba55ce6a43ad561768f8b455b60ac2e678956206
pkryazhev <pkryazhev@ozon.ru>
2021-11-18 21:08:57 +0300

    step 2

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,6 +5,8 @@ const run = () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
+    .argument('filepath1')
+    .argument('filepath2')
     .version('0.0.1');
   program.parse(process.argv);
 };
