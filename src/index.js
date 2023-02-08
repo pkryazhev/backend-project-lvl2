78d14dbda2dc5adf916feb8d474956dd8a20af84
pkryazhev <pkryazhev@ozon.ru>
2023-02-08 19:50:13 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import generateOutput from './formatters/index.js';
 import { compare, getDataFromFile } from './compareFile.js';
 
-export default (filePath1, filePath2, format) => {
+export default (filePath1, filePath2, format = 'stylish') => {
   const data1 = getDataFromFile(filePath1);
   const data2 = getDataFromFile(filePath2);
   const compareData = compare(data1, data2);
